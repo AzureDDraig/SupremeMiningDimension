2e666156f0e914ebb88912b008f78842d5b84cb5
AzureDDraig <107419858+AzureDDraig@users.noreply.github.com>
2023-08-28 12:22:02 -0500

    1.4.2.2 build 3 - Nuggets and titanium prep!
    Added nuggets for each metal
    Added preliminary recipes for titanium

---
+++ src/main/java/net/ddraig/suprememiningdimension/item/TitaniumNuggetItem.java
@@ -0,0 +1,13 @@
+
+package net.ddraig.suprememiningdimension.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+import net.ddraig.suprememiningdimension.init.SupremeMiningDimensionModTabs;
+
+public class TitaniumNuggetItem extends Item {
+	public TitaniumNuggetItem() {
+		super(new Item.Properties().tab(SupremeMiningDimensionModTabs.TAB_SUPREME_MINING_DIMENSIONS).stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
